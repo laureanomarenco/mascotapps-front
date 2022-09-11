93300ddb3c004d0f23f4fd9dc4a067905719e163
laureanomarenco <laureanomarenco@gmail.com>
2022-09-11 11:21:36 -0300

    first commit

---
+++ components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material'
+import Login from '../Login/Login'
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <Container>
+     <Login/>
+    </Container>
+  )
+}
