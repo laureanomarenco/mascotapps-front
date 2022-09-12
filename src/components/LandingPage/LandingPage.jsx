3ec98bb7765e7a7ee6d236103452bec6177af788
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-12 09:54:03 -0300

    borrando componentes de mui

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,7 @@
-import {Grid} from '@mui/material'
-import Login from '../Login/Login'
-import MasonryImageList from './Carrousel'
 import React from 'react'
 
 export default function LandingPage() {
-  
   return (
-    <Grid container columnGap={4}>
-      <Grid item xl={9} style={{position: 'absolute'}}>
-        <MasonryImageList/>
-      </Grid>
-      <Grid item xl={3}>
-       <Login/>  
-      </Grid>
-    </Grid>
+    <div>LandingPage</div>
   )
 }
