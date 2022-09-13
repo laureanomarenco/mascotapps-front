db462e787cf44819798bebe2afa62b0ee598f7bd
lucascolm <lucaspaganelli20@gmail.com>
2022-09-13 15:25:33 -0300

    hago un fix al comp navbar + searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,33 +1,15 @@
 import React from 'react'
+import {BiSearch}from "react-icons/bi"
 
 const SearchBar = () => {
   return (
-    <div className="flex justify-center">
-  <div className="mb-3 xl:w-96">
-    <input
-      type="search"
-      className="
-        form-control
-        block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      "
-      id="exampleSearch"
-      placeholder="Buscar"
-    />
-  </div>
-</div>
+    <div className="flex items-center">
+      <input className='sm:w-[400px] h-[35px] w-48  shadow-lg shadow-zinc-200 outline-none font-thin text-xl px-4 'placeholder="Buscar..." ></input>
+
+      <button className=" bg-zinc-100  w-[70px] h-[35px] rounded-lg font-thin shdow-lg  hover:text-indigo-600 hover:border-current"><BiSearch className='mx-auto' size="20px"/></button>
+
+      </div>
+    
   )
 }
 
