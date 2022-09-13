03cee51352c007366c739c8c992850878416cfed
laureanomarenco <laureanomarenco@gmail.com>
2022-09-13 09:05:01 -0300

    navbar y footer agregados a home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Navbar/>
+      <Footer />
+    </>
   )
 }
