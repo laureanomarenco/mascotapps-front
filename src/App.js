e904ab1e652dc41bb622229bacc2c585c6741a6a
Martín Fiori <martinfiori@outlook.com>
2022-09-14 16:28:04 -0300

    adding loading in cardContainer & details

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import './App.css';
+import "./App.css";
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import PetsContainer from './components/PetsContainer/PetsContainer';
-import Detail from './components/Detail/Detail';
-import Home from './components/Home/Home'
-import LandingPage from './components/LandingPage/LandingPage';
-import SignUp from './components/SignUp/SignUp';
-import Error from './components/Error/Error';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PetsContainer from "./components/PetsContainer/PetsContainer";
+import Detail from "./components/Detail/Detail";
+import Home from "./components/Home/Home";
+import LandingPage from "./components/LandingPage/LandingPage";
+import SignUp from "./components/SignUp/SignUp";
+import Error from "./components/Error/Error";
 
-import React from 'react';
-import UserProfile from './components/UserProfile/UserProfile';
+import React from "react";
+import UserProfile from "./components/UserProfile/UserProfile";
 
-import { fetchPets } from './store/actions';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+// import { fetchPets } from "./store/actions";
+// import { useDispatch } from "react-redux";
+// import { useEffect } from "react";
 
 function App() {
-  //eslint-disable-next-line
-  
-  let dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchPets());
-  }, [dispatch]);
-  return (
-  <div className='.App'>
-    {/* //   <header className="App-header">
+	//eslint-disable-next-line
+
+	// let dispatch = useDispatch();
+	// useEffect(() => {
+	//   dispatch(fetchPets());
+	// }, [dispatch]);
+	return (
+		<div className="App bg-[url('https://res.cloudinary.com/dax0wf30d/image/upload/v1663115601/shit/bg-5_nbb3sj.png')]">
+			{/* //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
     //     <div>
     //     {pets.map((pet, i) => { */}
-    {/* //       <div>
+			{/* //       <div>
     //         <p key={i}>{pet.name}</p>
     //         </div>
     //     })}
     //     </div>
     //   </header> */}
 
-  <BrowserRouter>
-    <Routes>
-      <Route exact path='/' element={<LandingPage />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/register" element={<SignUp />}/>
-      <Route path="/account" element={<UserProfile/>}/>
-      <Route path="/pets/:id" element={<Detail/>}/>
-      <Route path="/estado/:status" element={<PetsContainer />}/>
-      <Route path="*" element={<Error />} />
-    </Routes>
-  </BrowserRouter>
-  </div>
-  )
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<LandingPage />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/register" element={<SignUp />} />
+					<Route path="/account" element={<UserProfile />} />
+					<Route path="/pets/:id" element={<Detail />} />
+					<Route path="/estado/:status" element={<PetsContainer />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,33 +1,48 @@
-import { FETCH_PETS, GET_DETAIL, RESET_DETAIL,GET_PETS_BY_STATUS } from "../actions";
+import {
+	FETCH_PETS,
+	GET_DETAIL,
+	RESET_DETAIL,
+	GET_PETS_BY_STATUS,
+	SET_LOADING,
+} from "../actions";
 
 const initalState = {
-    pets: [],
-    pet:{},
-    statusPets:[]
-  };
-  
-  export default function reducer(state = initalState, action) {
-    switch (action.type) {
-      case FETCH_PETS:
-        return {
-          ...state,
-          pets: action.payload,
-        };
-      case GET_DETAIL:
-        return {
-          ...state, 
-          pet: action.payload
-        };
-      case RESET_DETAIL:
-        return {
-          pet: {}
-        };
-        case GET_PETS_BY_STATUS:
-          return{
-            ...state,
-            statusPets:action.payload 
-          };
-      default:
-        return state;
-    }
-  }
+	pets: [],
+	pet: {},
+	statusPets: [],
+	isLoading: true,
+};
+
+export default function reducer(state = initalState, action) {
+	switch (action.type) {
+		case FETCH_PETS:
+			return {
+				...state,
+				pets: action.payload,
+				isLoading: false,
+			};
+		case GET_DETAIL:
+			return {
+				...state,
+				pet: action.payload,
+				isLoading: false,
+			};
+		case RESET_DETAIL:
+			return {
+				...state,
+				pet: {},
+			};
+		case GET_PETS_BY_STATUS:
+			return {
+				...state,
+				statusPets: action.payload,
+			};
+		case SET_LOADING:
+			return {
+				...state,
+				isLoading: action.payload,
+			};
+		default:
+			return state;
+	}
+}
