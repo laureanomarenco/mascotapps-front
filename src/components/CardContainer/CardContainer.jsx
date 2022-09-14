b08234f9391fbf8a378c1e8ac4391cd3e954a709
laureanomarenco <laureanomarenco@gmail.com>
2022-09-14 20:12:04 -0300

    spinner-martin

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,21 +1,29 @@
-
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPets } from "../../store/actions/index";
 import Card from "../Card/Card";
+import Spinner from "../Spinner/Spinner";
 
 export default function CardContainer() {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.isLoading);
   const pets = useSelector((state) => state.pets);
   useEffect(() => {
     !pets.length && dispatch(fetchPets());
   }, []);
   return (
-    <div className="grid gap-1 grid-cols-1 grid-rows-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 bg-[url('https://res.cloudinary.com/dax0wf30d/image/upload/v1663115601/shit/bg-5_nbb3sj.png')]">
-      {pets.map((pet) => (
-        <Card key={pet.id} data={pet} />
-      ))}
-    </div> 
+    <div
+      className={`${
+        loading
+          ? "grid gap-1 grid-rows-1 pt-40"
+          : "grid gap-1 grid-rows-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4"
+      } grid-cols-1 min-h-[70vh]`}
+    >
+      {loading ? (
+        <Spinner />
+      ) : (
+        pets.map((pet) => <Card key={pet.id} data={pet} />)
+      )}
+    </div>
   );
-
 }
