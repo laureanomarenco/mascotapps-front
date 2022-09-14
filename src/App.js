f2231f601660ffcd7a1b3c49f44305372cd3be95
Martín Fiori <martinfiori@outlook.com>
2022-09-14 02:00:49 -0300

    adding error page

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import './App.css';
+import "./App.css";
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Detail from './components/Detail/Detail';
-import Home from './components/Home/Home'
-import LandingPage from './components/LandingPage/LandingPage';
-import Login from './components/Login/Login';
-import React from 'react';
-import UserProfile from './components/UserProfile/UserProfile';
-import { fetchPets } from './store/actions';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import Detail from "./components/Detail/Detail";
+import Home from "./components/Home/Home";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Login from "./components/Login/Login";
+import React from "react";
+import UserProfile from "./components/UserProfile/UserProfile";
+import Error from "./components/Error/Error.jsx";
+import { fetchPets } from "./store/actions";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
-  //eslint-disable-next-line
-  
-  let dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchPets());
-  }, [dispatch]);
-  return (
-  <div className='.App'>
-    {/* //   <header className="App-header">
+	//eslint-disable-next-line
+
+	let dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchPets());
+	}, [dispatch]);
+	return (
+		<div className=".App">
+			{/* //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
     //     <div>
     //     {pets.map((pet, i) => { */}
-    {/* //       <div>
+			{/* //       <div>
     //         <p key={i}>{pet.name}</p>
     //         </div>
     //     })}
     //     </div>
     //   </header> */}
 
-  <BrowserRouter>
-    <Routes>
-      <Route exact path='/' element={<LandingPage />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<Login />}/>
-      <Route path="/account" element={<UserProfile/>}/>
-      <Route path="/pets/:id" element={<Detail/>}/>
-    </Routes>
-  </BrowserRouter>
-  </div>
-  )
+			<BrowserRouter>
+				<Routes>
+					<Route path="/pets/:id" element={<Detail />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/account" element={<UserProfile />} />
+					<Route exact path="/" element={<LandingPage />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
