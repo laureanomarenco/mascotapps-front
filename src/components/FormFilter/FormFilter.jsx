7761112d0b733117aaafce17d0c082714db52965
lucascolm <lucaspaganelli20@gmail.com>
2022-09-16 14:26:36 -0300

    tamaraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/components/FormFilter/FormFilter.jsx
+++ src/components/FormFilter/FormFilter.jsx
@@ -34,27 +34,27 @@ const FormFilter = () => {
     });
   };
   return (
-    <div>
+    <div className="grid gap-1 grid-cols-1 md:grid-cols-2 xl:grid-cols-6 border-0 cursor-pointer rounded-full drop-shadow-md  w-70  duration-300 ">
       <select value={filter.specie} name="specie" onChange={handleFilter}>
-        <option hidden>Especie</option>
+        <option  hidden>Especie</option>
         <option value="perro">Perro</option>
         <option value="gato">Gato</option>
         <option value="otra especie">Otro</option>
       </select>
-      <select value={filter.gender} name="gender" onChange={handleFilter}>
-        <option hidden>Genero </option>
+      <select  value={filter.gender} name="gender" onChange={handleFilter}>
+        <option   hidden>Genero </option>
         <option value="macho">Macho</option>
         <option value="hembra">Hembra</option>
       </select>
-      <select value={filter.age} name="age" onChange={handleFilter}>
-        <option hidden>Edad </option>
+      <select  value={filter.age} name="age" onChange={handleFilter}>
+        <option   hidden>Edad </option>
         <option value="muy joven">Cachorro</option>
         <option value="joven">Joven</option>
         <option value="adulto">Adulto</option>
         <option value="viejo">Viejo</option>
       </select>
-      <select value={filter.race} name="race" onChange={handleFilter}>
-        <option hidden>Raza </option>
+      <select  value={filter.race} name="race" onChange={handleFilter}>
+        <option  hidden>Raza </option>
         {pets?.map((pet) => (
           <option key={pet.id} value={pet.race}>
             {pet.race}
@@ -64,7 +64,7 @@ const FormFilter = () => {
       <select>
         <option hidden>Ciudad</option>
       </select>
-      <button onClick={clear}>reset</button>
+      <button className="hover:bg-[#28B0A2]" onClick={clear}>reset</button>
     </div>
   );
 };
--- src/components/PetsContainer/PetsContainer.jsx
+++ src/components/PetsContainer/PetsContainer.jsx
@@ -4,6 +4,7 @@ import Card from "../Card/Card"
 import Navbar from "../Navbar/Navbar";
 import { useSelector } from 'react-redux';
 import FormFilter from '../FormFilter/FormFilter';
+import NotFound from '../NotFound/NotFound';
 const PetsContainer = () => {
 // const dispatch=useDispatch();
 // const {status}=useParams();
@@ -16,7 +17,7 @@ const notFound =useSelector(state=>state.notFound)
     <Navbar/>
     <FormFilter/>
     <div className=" grid gap-1 grid-cols-1 gird-rows-1 md:grid-cols-2 xl:gird-cols-3 2xl:grid-cols-3 bg-[url('https://res.cloudinary.com/dax0wf30d/image/upload/v1663115601/shit/bg-5_nbb3sj.png')]">
-    {notFound && <h1>no hay</h1>}
+    {notFound && <NotFound/>}
     { filterPets.length >0 &&!notFound?
        filterPets.map(fPet=>(<Card key={fPet.id} data={fPet}/>)) 
       :!notFound && pets?.map(pet=>( <Card key={pet.id} data={pet}/>) )
