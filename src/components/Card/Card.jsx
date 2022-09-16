f32e55e5358c1e17f77e29332d62e54016ee598d
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-16 13:49:13 -0300

    ahora si card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ data: { name, id, image, gender, age, specie, race } }) => {
 		<Link
 			to={'/pets/'+id}
 			key={id}
-			className="relative bg-[#F4F6F6] text-[#121212] my-8 mx-auto bg-600 max-h-[327px] w-80 rounded overflow-hidden shadow-md hover:shadow-lg cursor-pointer"
+			className="relative bg-[#F4F6F6] text-[#121212] my-8 mx-auto bg-600 max-h-[347px] w-80 rounded overflow-hidden shadow-md hover:shadow-lg cursor-pointer"
 		>
 			<h1 className="text-center py-3 capitalize bg-[#28B0A280] text-[#121212] border-white border-b-2 font-semibold">
 				{name ? name : "Mascota"}
@@ -24,7 +24,7 @@ const Card = ({ data: { name, id, image, gender, age, specie, race } }) => {
 			<span className="absolute top-14 right-2 bg-[#FFC700] p-1 rounded-lg text-[#121212] font-medium capitalize">
 				{gender}
 			</span>
-			<ul className="flex p-2 justify-evenly items-center divide-x divide-[#fff]">
+			<ul className="flex p-2 justify-evenly items-start divide-x divide-[#fff]">
 				<li className="w-full block text-center capitalize">
 					<span className="block text-center font-bold">especie: </span>
 					{specie}
