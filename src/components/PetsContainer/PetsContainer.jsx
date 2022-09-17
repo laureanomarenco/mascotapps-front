4057062444c1925b1f3ab9a5415f053b8b87ee03
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-17 16:03:11 -0300

    a ver a ver probandoooo

--- src/components/PetsContainer/PetsContainer.jsx
+++ src/components/PetsContainer/PetsContainer.jsx
@@ -1,15 +1,14 @@
-
 import React, { useState } from "react";
 
 import Card from "../Card/Card";
 // import Footer from "../Footer/Footer";
-import Pagination from "../Pagination/Pagination";
+
 import Navbar from "../Navbar/Navbar";
 
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 import { useDispatch, useSelector } from "react-redux";
 import FormFilter from "../FormFilter/FormFilter";
-import { resetDetail, filterPets} from "../../store/actions";
+import { resetDetail, filterPets } from "../../store/actions";
 
 const PetsContainer = () => {
   // const dispatch=useDispatch();
@@ -25,7 +24,7 @@ const PetsContainer = () => {
     age: "",
     race: "",
   });
-    
+
   const handleFilter = (e) => {
     setFilter({
       ...filter,
@@ -33,19 +32,18 @@ const PetsContainer = () => {
     });
     const obj = {
       ...filter,
-      [e.target.name] : e.target.value
-    }
+      [e.target.name]: e.target.value,
+    };
     dispatch(filterPets(obj));
   };
-  const showAlert=()=>{
+  const showAlert = () => {
     Swal.fire({
-      title:'Error!',
-      text:'No pudimos encontrar una mascota con esas características',
-      icon:'error',
-      confirmButtonText:'Ok'
-    })
-    .then(()=>handleClearFilter())
-  }
+      title: "Error!",
+      text: "No pudimos encontrar una mascota con esas características",
+      icon: "error",
+      confirmButtonText: "Ok",
+    }).then(() => handleClearFilter());
+  };
   const handleClearFilter = () => {
     dispatch(resetDetail());
     setFilter({
@@ -58,15 +56,18 @@ const PetsContainer = () => {
   return (
     <div>
       <Navbar />
-      <FormFilter handleClearFilter={handleClearFilter} filter={filter} handleFilter={handleFilter}/>
-        {notFound && showAlert() }
+      <FormFilter
+        handleClearFilter={handleClearFilter}
+        filter={filter}
+        handleFilter={handleFilter}
+      />
+      {notFound && showAlert()}
       <div className=" grid gap-1 grid-cols-1 gird-rows-1 md:grid-cols-2 xl:gird-cols-3 2xl:grid-cols-3 bg-[url('https://res.cloudinary.com/dax0wf30d/image/upload/v1663115601/shit/bg-5_nbb3sj.png')]">
         {filterPet.length > 0 && !notFound
           ? filterPet.map((fPet) => <Card key={fPet.id} data={fPet} />)
           : !notFound && pets?.map((pet) => <Card key={pet.id} data={pet} />)}
       </div>
       {/* <Footer/> */}
-
     </div>
   );
 };
