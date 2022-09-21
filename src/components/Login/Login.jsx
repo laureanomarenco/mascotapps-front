78fe37b7d71494002b24d01aafaba729d0fdefba
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-21 13:35:05 -0300

    logout component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,14 +3,15 @@ import React from "react";
 
 import { LOGIN } from "../../url/url";
 //eslint-disable-next-line
-import { LoginButton } from "./LoginButton";
-//eslint-disable-next-line
 import { Logout } from "../Logout/Logout";
+//eslint-disable-next-line
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Login() {
   //eslint-disable-next-line
-  const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
+    useAuth0();
   //eslint-disable-next-line
   const navigate = useNavigate();
 
@@ -18,6 +19,12 @@ export default function Login() {
   const googleLogin = () => {
     window.open(`${LOGIN}`, "_self");
   };
+  function handleValidation (){
+    if (isAuthenticated) {
+      navigate("/home");
+    }
+  }
+  handleValidation()
 
   return (
     <>
@@ -112,30 +119,56 @@ export default function Login() {
               </a>
             </div> */}
 
-              <div>
-                <button
-                  type="submit"
-                  onClick={() => loginWithRedirect()}
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#ecca08] py-2 px-4 text-sm font-medium text-black hover:bg-[#ffd903]  focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
-                >
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg
-                      className="h-5 w-5 text-black group-hover:text-[#0f0f0f]"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                  Ingresa a tu cuenta
-                </button>
-              </div>
+              {isAuthenticated ? (
+                <div>
+                  <button
+                    onClick={() => logout({ returnTo: window.location.origin })}
+                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#ecca08] py-2 px-4 text-sm font-medium text-black hover:bg-[#ffd903]  focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
+                  >
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                      <svg
+                        className="h-5 w-5 text-black group-hover:text-[#0f0f0f]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    Cerrar sesión
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <button
+                    type="submit"
+                    onClick={() => loginWithRedirect()}
+                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#ecca08] py-2 px-4 text-sm font-medium text-black hover:bg-[#ffd903]  focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
+                  >
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                      <svg
+                        className="h-5 w-5 text-black group-hover:text-[#0f0f0f]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    Ingresa a tu cuenta
+                  </button>
+                </div>
+              )}
             </form>
           </div>
           <div>
