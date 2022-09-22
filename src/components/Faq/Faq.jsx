8d718aad3058f48f1efcee77d194a4023ec9fb35
Gonzalo <fonsofara@gmail.com>
2022-09-22 11:28:09 -0300

    otra pregunta en faq y values de cities

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -135,6 +135,35 @@ const Faq = () => {
           distinctio corporis earum similique!
         </p>
       </details>
+      <details className="relative p-6 border-l-4 border-[#FFC700] bg-gray-100 group drop-shadow-lg overflow-hidden bg-no-repeat bg-cover transition duration-300 ease-in-out hover:bg-gradient-to-r from-transparent via-yellow-200-100 to-[#FFC700] hover:opacity-70 mt-0 mb-4">
+        <summary className="flex items-center justify-between cursor-pointer">
+          <h5 className="text-lg font-semibold text-gray-900">
+            ¿Qué hago si recuperé o adoptaron mi mascota?
+          </h5>
+
+          <span className="flex-shrink-0 ml-1.5 p-1.5 text-gray-900 bg-white rounded-full sm:p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="flex-shrink-0 w-5 h-5 transition duration-300 group-open:-rotate-45"
+              viewBox="0 0 20 20"
+              fill="#28B0A2"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+        </summary>
+
+        <p className="mt-4 leading-relaxed text-gray-700">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+          veritatis molestias culpa in, recusandae laboriosam neque aliquid
+          libero nesciunt voluptate dicta quo officiis explicabo consequuntur
+          distinctio corporis earum similique!
+        </p>
+      </details>
 
       <div className="h-[50px] mt-6 w-2/6 mx-auto text-center">
         <Link
