089bbf39ebe6896eac1507446712b48ac40b0bc2
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-22 21:39:37 -0300

    okok

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,110 +1,124 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import React from "react";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 //eslint-disable-next-line
 import { Logout } from "../Logout/Logout";
 //eslint-disable-next-line
+import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "./LoginButton";
+import Spinner from "../Spinner/Spinner";
+import { useState } from "react";
 
 export default function Login() {
   //eslint-disable-next-line
   const { user, isAuthenticated, loginWithRedirect } = useAuth0();
-  //eslint-disable-next-line
+  const [load, setLoad] = useState(false);
+
   const navigate = useNavigate();
-  // eslint-disable-next-line
   const handleValidation = async (user, isAuthenticated) => {
     try {
       if (isAuthenticated && user) {
+        setLoad(true);
         let existe = await axios.post(
           "https://mascotapps-back-dev-2.up.railway.app/users/exists ",
           {
             id: user?.sub,
           }
         );
-        console.log(
-          "🚀 ~ file: Login.jsx ~ line 25 ~ handleValidation ~ existe",
-          existe
-        );
-        console.log("respuesta del post ", existe.data);
-        if (existe.data.msg) {
-          navigate("/home");
-        } else {
-          navigate("/register");
+        if (existe.data) {
+          setLoad(false);
+          console.log("respuesta del post ", existe.data);
+          if (existe.data.msg) {
+            navigate("/home");
+          } else {
+            navigate("/register");
+          }
         }
       }
     } catch (error) {
       console.log(error);
     }
   };
-
-  handleValidation(user, isAuthenticated);
-
-  return (
-    <>
-      <div className="absolute w-full h-full items-center  justify-center  py-12 px-4 md:px-6 lg:px-8 md:w-1/3 lg:w-1/4 md:h-full md:right-10 bg-white">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <img
-              className="mx-auto h-48 w-auto"
-              src="https://res.cloudinary.com/dfbxjt69z/image/upload/v1663007100/mascotapps/mascotapss_zihxad.png"
-              alt="Your Company"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Bienvenidos a Mascotapp
-            </h2>
-            <form className="mt-8 space-y-6" action="#" method="POST">
-              {isAuthenticated ? (
-                <div>
-                  <Logout />
-                </div>
-              ) : (
-                <div>
-                  <button
-                    type="submit"
-                    onClick={() => loginWithRedirect()}
-                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#ecca08] py-2 px-4 text-sm font-medium text-black hover:bg-[#ffd903]  focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
-                  >
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                      <svg
-                        className="h-5 w-5 text-black group-hover:text-[#0f0f0f]"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                    Ingresa a tu cuenta
-                  </button>
-                </div>
-              )}
-            </form>
-          </div>
-          <div>
-            <p className="mt-2 text-center  text-gray-600 ">
-              <Link to="/home">
-                <a className="font-medium text-[#28B0A2] hover:text-[#208a7f]">
-                  {" "}
-                  Navegar sin registrarse
-                </a>
-              </Link>
-            </p>
-            <p className="mt-2 text-center  text-gray-600">
-              <Link to="/faqs">
-                <a className="font-medium text-[#28B0A2] hover:text-[#208a7f]">
-                  {" "}
-                  FAQs
-                </a>
-              </Link>
-            </p>
+  if (isAuthenticated) {
+    handleValidation(user, isAuthenticated);
+  }
+  if (!load) {
+    return (
+      <>
+        <div className="absolute w-full h-full items-center  justify-center  py-12 px-4 md:px-6 lg:px-8 md:w-1/3 lg:w-1/4 md:h-full md:right-10 bg-white">
+          <div className="w-full max-w-md space-y-8">
+            <div>
+              <img
+                className="mx-auto h-48 w-auto"
+                src="https://res.cloudinary.com/dfbxjt69z/image/upload/v1663007100/mascotapps/mascotapss_zihxad.png"
+                alt="Your Company"
+              />
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Bienvenidos a Mascotapp
+              </h2>
+              <form className="mt-8 space-y-6" action="#" method="POST">
+                {isAuthenticated ? (
+                  <div>
+                    <Logout />
+                  </div>
+                ) : (
+                  <div>
+                    <LoginButton
+                      text="Ingresa a tu cuenta"
+                      className="group relative flex
+                    w-full justify-center rounded-md border border-transparent
+                    bg-[#ecca08] py-2 px-4 text-sm font-medium text-black
+                    hover:bg-[#ffd903] focus:outline-none focus:ring-2
+                    focus:ring-amber-500 focus:ring-offset-2"
+                    >
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                        <svg
+                          className="h-5 w-5 text-black group-hover:text-[#0f0f0f]"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </span>
+                    </LoginButton>
+                  </div>
+                )}
+              </form>
+            </div>
+            <div>
+              <p className="mt-2 text-center  text-gray-600 ">
+                <Link to="/home">
+                  <a className="font-medium text-[#28B0A2] hover:text-[#208a7f]">
+                    {" "}
+                    Navegar sin registrarse
+                  </a>
+                </Link>
+              </p>
+              <p className="mt-2 text-center  text-gray-600">
+                <Link to="/faqs">
+                  <a className="font-medium text-[#28B0A2] hover:text-[#208a7f]">
+                    {" "}
+                    FAQs
+                  </a>
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <Spinner />
       </div>
-    </>
-  );
+    );
+  }
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { getPetsByStatus } from "../../store/actions/index";
 import { useDispatch } from "react-redux";
@@ -104,7 +104,7 @@ export default function Navbar() {
                   <span className="sr-only"> Account </span>
                 </Link>
               ) : (
-                <LoginButton />
+                <LoginButton text="Iniciar sesión" />
               )}
 
               <Link
@@ -279,7 +279,7 @@ export default function Navbar() {
                       <span className="sr-only"> Account </span>
                     </Link>
                   ) : (
-                    <LoginButton />
+                    <LoginButton text="Iniciar sesión" />
                   )}
                 </div>
                 <div className="flex lg:hidden">
@@ -359,6 +359,7 @@ export default function Navbar() {
               </div>
             </div>
           </div>
+
           {/* MOBILE*/}
           <div
             id="mobile-menu"
@@ -548,46 +549,6 @@ export default function Navbar() {
             </div>
             <div className="h-full flex items-end">
               <ul className="flex flex-col space-y-8 bg-gray-50 w-full py-10 p-4 ">
-                {/* <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="text-gray-800 flex items-center space-x-2 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
-                  >
-                    <div>
-                      <svg
-                        className="fill-stroke"
-                        width={22}
-                        height={22}
-                        viewBox="0 0 22 22"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M4.33333 1L1 5V19C1 19.5304 1.23413 20.0391 1.65087 20.4142C2.06762 20.7893 2.63285 21 3.22222 21H18.7778C19.3671 21 19.9324 20.7893 20.3491 20.4142C20.7659 20.0391 21 19.5304 21 19V5L17.6667 1H4.33333Z"
-                          stroke="currentColor"
-                          strokeWidth="1.25"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                        <path
-                          d="M1 5H21"
-                          stroke="currentColor"
-                          strokeWidth="1.25"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                        <path
-                          d="M15.4436 9C15.4436 10.0609 14.9753 11.0783 14.1418 11.8284C13.3083 12.5786 12.1779 13 10.9991 13C9.82039 13 8.68993 12.5786 7.85643 11.8284C7.02294 11.0783 6.55469 10.0609 6.55469 9"
-                          stroke="currentColor"
-                          strokeWidth="1.25"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                    </div>
-                    <p className="text-base">Cart</p>
-                  </a>
-                </li> */}
                 {isAuthenticated ? (
                   <Link
                     to="/account"
@@ -613,7 +574,7 @@ export default function Navbar() {
                     <p className="text-base">Mi perfil</p>
                   </Link>
                 ) : (
-                  <LoginButton />
+                  <LoginButton text='Iniciar sesión' />
                 )}
 
                 <li>
