8ff7b95a7cd469ac3577dfabeca5cfef3aa2c13d
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-25 11:50:25 -0300

    map de transaction

--- src/components/UserProfile/Transactions/Transactions.jsx
+++ src/components/UserProfile/Transactions/Transactions.jsx
@@ -3,8 +3,8 @@ import { MdOutlineEdit } from "react-icons/md";
 import { GrView } from "react-icons/gr";
 import { RiDeleteBinLine } from "react-icons/ri";
 
-const Transactions = ({ myProfileData }) => {
-  console.log(myProfileData);
+const Transactions = ({ transactions }) => {
+  console.log(transactions);
   return (
     <div className=" rounded-md w-full">
       <div>
@@ -31,57 +31,64 @@ const Transactions = ({ myProfileData }) => {
                 </tr>
               </thead>
               <tbody>
-                <>
-                  <tr key={myProfileData.name}>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div className="flex items-center">
-                        <img
-                          className="w-12 h-12 rounded-full"
-                          src={myProfileData.image}
-                          alt="user-img"
-                        />
+                {transactions?.map((transaction) => (
+                  <>
+                    <tr key={transaction?.id}>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div className="flex items-center">
+                          <img
+                            className="w-12 h-12 rounded-full"
+                            src={transaction?.image}
+                            alt="user-img"
+                          />
 
-                        <div className="ml-3 grid">
-                          <p className="text-gray-900 whitespace-no-wrap capitalize">
-                            {myProfileData.name}
-                          </p>
-                          <p className="text-gray-400 whitespace-no-wrap">
-                            {myProfileData.email}
-                          </p>
+                          <div className="ml-3 grid">
+                            <p className="text-gray-900 whitespace-no-wrap capitalize">
+                              {transaction?.name}
+                            </p>
+                            <p className="text-gray-400 whitespace-no-wrap">
+                              {transaction?.email}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    {/* <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            {u.createdAt.slice(0, 10)}
-                          </p>
-                        </td> */}
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {myProfileData.city}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">43</p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                        <span
-                          aria-hidden
-                          className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                        ></span>
-                        <span className="relative">Activo</span>
-                      </span>
-                    </td>
-                    <td className=" py-5 border-b border-gray-200 bg-white text-sm">
-                      <div className="flex gap-3">
-                        <GrView size={22} />
-                        <MdOutlineEdit size={22} />
-                        <RiDeleteBinLine size={22} />
-                      </div>
-                    </td>
-                  </tr>
-                </>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {transaction?.city}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">43</p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight text-center">
+                          <span
+                            aria-hidden
+                            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                          ></span>
+                          {transaction.status === "active" && (
+                            <span className="relative">Activo</span>
+                          )}
+                          {transaction.status === "finalizado" && (
+                            <span className="relative">
+                              Calificación pendiente
+                            </span>
+                          )}
+                          {transaction.status === "reviewed" && (
+                            <span className="relative">Finzalizado</span>
+                          )}
+                        </span>
+                      </td>
+                      <td className=" py-5 border-b border-gray-200 bg-white text-sm">
+                        <div className="flex gap-3">
+                          <GrView size={22} />
+                          <MdOutlineEdit size={22} />
+                          <RiDeleteBinLine size={22} />
+                        </div>
+                      </td>
+                    </tr>
+                  </>
+                ))}
               </tbody>
             </table>
           </div>
