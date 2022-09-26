f12de7d1865a962054246daa9b79c8f78ff8a90c
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-26 19:12:09 -0300

    ahora si, sin bug de render infinito

--- src/components/UserProfile/ModalEditDog/ModalEditDog.jsx
+++ src/components/UserProfile/ModalEditDog/ModalEditDog.jsx
@@ -4,240 +4,234 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchCity, getSpecies, updatePet } from "../../../store/actions";
 import Swal from "sweetalert2";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 import validate from "../../PostPets/validate";
 
 export default function ModalEditDog({ handleActiveEditDog, dataEditDog }) {
-	const dispatch = useDispatch();
-	const PetSpecies = useSelector(state => state.species);
-	const { user } = useAuth0();
-	const [error, setError] = useState({});
-	const [input, setInput] = useState({ ...dataEditDog });
-	const showAlert = () => {
-		Swal.fire({
-			title: "Gracias!",
-			text: "Tu mascota fue actualizada con exito",
-			icon: "success",
-			confirmButtonText: "Ok",
-		}).then(result => {
-			if (result.isConfirmed) {
-				Navigate("/home");
-			}
-		});
-	};
-	const showError = () => {
-		Swal.fire({
-			title: "Error!",
-			text: "verifique los campos",
-			icon: "error",
-			confirmButtonText: "Ok",
-		});
-	};
+  const dispatch = useDispatch();
+  const PetSpecies = useSelector((state) => state.species);
+  const { user } = useAuth0();
+  const [error, setError] = useState({});
+  const [input, setInput] = useState({ ...dataEditDog });
+  const showAlert = () => {
+    Swal.fire({
+      title: "Gracias!",
+      text: "Tu mascota fue actualizada con exito",
+      icon: "success",
+      confirmButtonText: "Ok",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        handleActiveEditDog();
+      }
+    });
+  };
+  const showError = () => {
+    Swal.fire({
+      title: "Error!",
+      text: "verifique los campos",
+      icon: "error",
+      confirmButtonText: "Ok",
+    });
+  };
 
-	const CLOUD_NAME = "imagenes";
-	const UPLOAD_PRESET = "dpxrr2uyq";
+  const CLOUD_NAME = "imagenes";
+  const UPLOAD_PRESET = "dpxrr2uyq";
 
-	const upload = async e => {
-		const img = e.target.files[0];
-		const data = new FormData();
-		data.append("file", img);
-		data.append("upload_preset", CLOUD_NAME);
-		const response = await fetch(
-			`https://api.cloudinary.com/v1_1/${UPLOAD_PRESET}/image/upload`,
-			{ method: "POST", body: data }
-		);
-		const dataNew = await response.json();
-		setInput({
-			...input,
-			image: dataNew.secure_url,
-		});
-	};
+  const upload = async (e) => {
+    const img = e.target.files[0];
+    const data = new FormData();
+    data.append("file", img);
+    data.append("upload_preset", CLOUD_NAME);
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${UPLOAD_PRESET}/image/upload`,
+      { method: "POST", body: data }
+    );
+    const dataNew = await response.json();
+    setInput({
+      ...input,
+      image: dataNew.secure_url,
+    });
+  };
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		if (Object.values(error).length) {
-			showError();
-		} else {
-			if (input.name === "") {
-				setInput({
-					...input,
-					name: undefined,
-				});
-			}
-			dispatch(updatePet(user, input));
-			showAlert();
-			setInput({});
-		}
-	};
-	const handleChange = e => {
-		setInput({
-			...input,
-			[e.target.name]: e.target.value,
-		});
-		setError(
-			validate({
-				...input,
-				[e.target.name]: e.target.value,
-			})
-		);
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.values(error).length) {
+      showError();
+    } else {
+      if (input.name === "") {
+        setInput({
+          ...input,
+          name: undefined,
+        });
+      }
+      dispatch(updatePet(user, input));
+      showAlert();
 
-	useEffect(() => {
-		dispatch(fetchCity());
-		dispatch(getSpecies());
-	}, [dispatch]);
-	return (
-		<>
-			<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-				<div className="relative w-auto my-6 mx-auto max-w-3xl">
-					<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-						<div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-							<h3 className="text-3xl font-semibold">
-								Edita los datos de tu mascota
-							</h3>
-							<button onClick={()=> console.log(error)}>errores</button>
-						</div>
-						<div className="relative p-6 flex-auto">
-							<form
-								onSubmit={handleSubmit}
-								className="max-w-md mx-auto mt-8 mb-0 space-y-2"
-							>
-								<input
-									type="text"
-									name="name"
-									placeholder="Nombre"
-									value={input.name}
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
-									onChange={handleChange}
+      setInput({});
+    }
+  };
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    setError(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
+  useEffect(() => {
+    dispatch(fetchCity());
+    dispatch(getSpecies());
+  }, [dispatch]);
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-3 mx-auto max-w-3xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-3xl font-semibold">
+                Edita los datos de tu mascota
+              </h3>
+            </div>
+            <div className="relative p-6 flex-auto">
+              <form
+                onSubmit={handleSubmit}
+                className="max-w-md mx-auto mt-8 mb-0 space-y-2"
+              >
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Nombre"
+                  value={input.name}
+                  className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
+                  onChange={handleChange}
+                />
+								{error.name ? <span>{error.name}</span> : <span></span>}
+								<div className="flex">
+                <select
+                  onChange={handleChange}
+                  className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                  name="specie"
+                  value={input.specie}
+                >
+                  <option hidden>Especie</option>
+                  {PetSpecies?.map((especie) => (
+                    <option
+                      className="capitalize"
+                      key={Math.random()}
+                      value={especie}
+                    >
+                      {especie}
+                    </option>
+                  ))}
+                </select>
+                <input
+                  type="text"
+                  name="race"
+                  placeholder="Raza"
+                  value={input.race}
+                  className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
+                  onChange={handleChange}
 								/>
-								{error.name ? (
-									<span>{error.name}</span>
-								) : (
-									<span></span>
-								)}
-								<select
-									onChange={handleChange}
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-									name="specie"
-									value={input.specie}
-								>
-									<option hidden>Especie</option>
-									{PetSpecies?.map(especie => (
-										<option
-											className="capitalize"
-											key={Math.random()}
-											value={especie}
-										>
-											{especie}
-										</option>
-									))}
-								</select>
-								<input
-									type="text"
-									name="race"
-									placeholder="Raza"
-									value={input.race}
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
-									onChange={handleChange}
-								/>
-								<select
-									onChange={handleChange}
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-									name="status"
-									value={input.status}
-								>
-									<option hidden>Estado</option>
-									<option value="perdido">Perdido</option>
-									<option value="en adopción">
-										Adopcion
-									</option>
-									<option value="encontrado">
-										Encontrado
-									</option>
-								</select>
-								<select
-									onChange={handleChange}
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-									name="gender"
-									value={input.gender}
-								>
-									<option hidden>Genero</option>
-									<option value="macho">Macho</option>
-									<option value="hembra">Hembra</option>
-								</select>
-								<select
-									onChange={handleChange}
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-									name="age"
-									value={input.age}
-								>
-									<option hidden>Edad</option>
-									<option value="muy joven">Cachorro</option>
-									<option value="joven">Joven</option>
-									<option value="adulto">Adulto</option>
-									<option value="viejo">Adulto Mayor</option>
-									<option value="desconocido">
-										Desconocido
-									</option>
-								</select>
-								<select
-									onChange={handleChange}
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-									name="vaccinationSchemeStatus"
-									value={input.vaccinationSchemeStatus}
-								>
-									<option hidden>Vacunacion</option>
-									<option value="completo">Completo</option>
-									<option value="incompleto">
-										Incompleto
-									</option>
-									<option value="desconocido">
-										Desconocido
-									</option>
-								</select>
-								<input
-									type="file"
-									accept=".png, .jpg, .jpeg"
-									name="image"
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-									placeholder="Imagen"
-									onChange={upload}
-								/>
-								<textarea
-									onChange={handleChange}
-									type="text"
-									name="comments"
-									className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
-									placeholder="Descripcion de la mascota..."
-									value={input.comments}
-								/>
-									{!error.comments ? null : (
-										<span className="block text-center text-xs text-red-500">*{error.comments}</span>
-									)}
-								{/* codear antes de esto */}
-								<div className="flex items-center justify-between">
-									<button
-										type="submit"
-										className="w-full rounded-md border border-transparent bg-[#ecca08] py-2  text-sm font-medium text-black hover:bg-[#ffd903]  focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
-									>
-										Modificar los datos
-									</button>
 								</div>
-							</form>
-						</div>
-						<div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-							<button
-								className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-								type="button"
-								onClick={handleActiveEditDog}
-							>
-								Cerrar
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-		</>
-	);
+                <div className="flex">
+                  <select
+                    onChange={handleChange}
+                    className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                    name="status"
+                    value={input.status}
+                  >
+                    <option hidden>Estado</option>
+                    <option value="perdido">Perdido</option>
+                    <option value="en adopción">Adopcion</option>
+                    <option value="encontrado">Encontrado</option>
+                  </select>
+                  <select
+                    onChange={handleChange}
+                    className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                    name="gender"
+                    value={input.gender}
+                  >
+                    <option hidden>Genero</option>
+                    <option value="macho">Macho</option>
+                    <option value="hembra">Hembra</option>
+                  </select>
+                </div>
+                <div className="flex">
+                  <select
+                    onChange={handleChange}
+                    className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                    name="age"
+                    value={input.age}
+                  >
+                    <option hidden>Edad</option>
+                    <option value="muy joven">Cachorro</option>
+                    <option value="joven">Joven</option>
+                    <option value="adulto">Adulto</option>
+                    <option value="viejo">Adulto Mayor</option>
+                    <option value="desconocido">Desconocido</option>
+                  </select>
+                  <select
+                    onChange={handleChange}
+                    className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                    name="vaccinationSchemeStatus"
+                    value={input.vaccinationSchemeStatus}
+                  >
+                    <option hidden>Vacunacion</option>
+                    <option value="completo">Completo</option>
+                    <option value="incompleto">Incompleto</option>
+                    <option value="desconocido">Desconocido</option>
+                  </select>
+                </div>
+                <input
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  name="image"
+                  className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                  placeholder="Imagen"
+                  onChange={upload}
+                />
+                <textarea
+                  onChange={handleChange}
+                  type="text"
+                  name="comments"
+                  className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
+                  placeholder="Descripcion de la mascota..."
+                  value={input.comments}
+                />
+                {!error.comments ? null : (
+                  <span className="block text-center text-xs text-red-500">
+                    *{error.comments}
+                  </span>
+                )}
+                {/* codear antes de esto */}
+                <div className="flex items-center justify-between">
+                  <button
+                    type="submit"
+                    className="w-full rounded-md border border-transparent bg-[#ecca08] py-2  text-sm font-medium text-black hover:bg-[#ffd903]  focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
+                  >
+                    Modificar los datos
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="flex items-center justify-end p-3 border-t border-solid border-slate-200 rounded-b">
+              <button
+                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={handleActiveEditDog}
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
 }
--- src/components/UserProfile/NuevoProfile.jsx
+++ src/components/UserProfile/NuevoProfile.jsx
@@ -54,13 +54,13 @@ const NuevoProfile = () => {
 	const handleActiveEditDog = data => {
 		setActiveModalEditDog(!activeModalEditDog);
 		data && setDataEditDog(data);
+		setOrder(order === "now" ? "nowpAPASITO" : "now");
 	};
 
 
   useEffect(() => {
     dispatch(myProfile({ id: user?.sub }));
 		handleSubmit();
-		setOrder(order === "now" ? "nowpAPASITO" : "now");
     return () => {
       dispatch(resetMyProfile());
       dispatch(resetDetail());
@@ -95,140 +95,133 @@ const NuevoProfile = () => {
 		});
 	}
 	return (
-		<div>
-			<Navbar></Navbar>
-			<div className=" my-5 mx-5 p-3">
-				<div className="md:flex no-wrap relative">
-					{myProfileData["userProps"]?.isDonator === "true" && (
-						<div className="absolute transition-all ease-in-out  duration-250	 cursor-pointer text-teal-500 hover:text-amber-400 top-5 right-0 text-4xl">
-							<BiDonateHeart />
-						</div>
-					)}
-					{/* <!-- Left Side --> */}
-					<div className="w-full md:w-1/2 md:mx-6 lg:w-3/12 lg:mx-8">
-						{/* <!-- Profile Card --> */}
-						<div className="bg-white p-3 border-t-4 border-[#FFC700]">
-							<div className="image overflow-hidden w-[280px] h-[280px] mx-auto md:h-[200px] md:w-[200px]">
-								<img
-									className="h-full w-full  mx-auto rounded-full object-cover"
-									src={myProfileData["userProps"]?.image}
-									alt=""
-								/>
-							</div>
-							<h1 className="text-gray-600 font-bold text-xl leading-8 my-1 text-center">
-								{myProfileData["userProps"]?.name}
-							</h1>
+    <div>
+      <Navbar></Navbar>
+      <div className=" my-5 mx-5 p-3">
+        <div className="md:flex no-wrap relative">
+          {myProfileData["userProps"]?.isDonator === "true" && (
+            <div className="absolute transition-all ease-in-out  duration-250	 cursor-pointer text-teal-500 hover:text-amber-400 top-5 right-0 text-4xl">
+              <BiDonateHeart />
+            </div>
+          )}
+          {/* <!-- Left Side --> */}
+          <div className="w-full md:w-1/2 md:mx-6 lg:w-3/12 lg:mx-8">
+            {/* <!-- Profile Card --> */}
+            <div className="bg-white p-3 border-t-4 border-[#FFC700]">
+              <div className="image overflow-hidden w-[280px] h-[280px] mx-auto md:h-[200px] md:w-[200px]">
+                <img
+                  className="h-full w-full  mx-auto rounded-full object-cover"
+                  src={myProfileData["userProps"]?.image}
+                  alt=""
+                />
+              </div>
+              <h1 className="text-gray-600 font-bold text-xl leading-8 my-1 text-center">
+                {myProfileData["userProps"]?.name}
+              </h1>
 
-							<ul className=" mt-3 divide-y ">
-								<li className="grid items-center text-center py-3 gap-1">
-									<Link
-										to="/postpets"
-										className="px-6 py-3  bg-[#FFC700] rounded-md font-bold hover:bg-[#ffd803]/80 transition-all duration-300"
-									>
-										Postear un aviso!
-									</Link>
+              <ul className=" mt-3 divide-y ">
+                <li className="grid items-center text-center py-3 gap-1">
+                  <Link
+                    to="/postpets"
+                    className="px-6 py-3  bg-[#FFC700] rounded-md font-bold hover:bg-[#ffd803]/80 transition-all duration-300"
+                  >
+                    Postear un aviso!
+                  </Link>
 
-									<button
-										className="px-6 py-3 bg-[#FFC700] rounded-md font-bold hover:bg-[#ffd803]/80 transition-all duration-300"
-										onClick={handleClick}
-									>
-										{" "}
-										Ver mis mascotas!
-									</button>
-									<div className="w-full">
-										<Logout />
-									</div>
-								</li>
-							</ul>
-						</div>
-						{/* <!-- End of profile card --> */}
-					</div>
-					{/* <!-- Right Side --> */}
-					<div className="w-full lg:w-9/12 lg:mx-8 mx-2 ">
-						{/* <!-- Profile tab -->
+                  <button
+                    className="px-6 py-3 bg-[#FFC700] rounded-md font-bold hover:bg-[#ffd803]/80 transition-all duration-300"
+                    onClick={handleClick}
+                  >
+                    {" "}
+                    Ver mis mascotas!
+                  </button>
+                  <div className="w-full">
+                    <Logout />
+                  </div>
+                </li>
+              </ul>
+            </div>
+            {/* <!-- End of profile card --> */}
+          </div>
+          {/* <!-- Right Side --> */}
+          <div className="w-full lg:w-9/12 lg:mx-8 mx-2 ">
+            {/* <!-- Profile tab -->
                 <!-- About Section --> */}
-						<div className="bg-white p-3 shadow-sm rounded-sm">
-							<div className="flex items-center space-x-2 font-semibold text-gray-700 leading-8">
-								<FaUser />
-								<span className="tracking-wide">
-									Información de perfil
-								</span>
-								<div>
-									<ModalProfile belloPerfil={belloPerfil} />
-								</div>
-							</div>
-							<div className="text-gray-700">
-								<div className="grid md:grid-cols-1 text-sm lg:grid-cols-2">
-									<div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
-										<div className=" py-2 font-semibold text-[#28B0A2]">
-											Nombre
-										</div>
-										<div className=" py-2 text-gray-400 col-span-2">
-											{myProfileData["userProps"]?.name}
-										</div>
-									</div>
-									<div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
-										<div className=" py-2 font-semibold text-[#28B0A2]">
-											Contacto
-										</div>
-										<div className=" py-2 text-gray-400 col-span-2">
-											{
-												myProfileData["userProps"]
-													?.contact
-											}
-										</div>
-									</div>
-									<div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
-										<div className=" py-2 font-semibold text-[#28B0A2]">
-											Email
-										</div>
-										<div className=" py-2 text-gray-400 col-span-2">
-											{belloPerfil?.email}
-										</div>
-									</div>
-									<div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
-										<div className=" py-2 font-semibold text-[#28B0A2]">
-											Ciudad
-										</div>
-										<div className=" py-2 text-gray-400 col-span-2">
-											{myProfileData["userProps"]?.city}
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+            <div className="bg-white p-3 shadow-sm rounded-sm">
+              <div className="flex items-center space-x-2 font-semibold text-gray-700 leading-8">
+                <FaUser />
+                <span className="tracking-wide">Información de perfil</span>
+                <div>
+                  <ModalProfile belloPerfil={belloPerfil} />
+                </div>
+              </div>
+              <div className="text-gray-700">
+                <div className="grid md:grid-cols-1 text-sm lg:grid-cols-2">
+                  <div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
+                    <div className=" py-2 font-semibold text-[#28B0A2]">
+                      Nombre
+                    </div>
+                    <div className=" py-2 text-gray-400 col-span-2">
+                      {myProfileData["userProps"]?.name}
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
+                    <div className=" py-2 font-semibold text-[#28B0A2]">
+                      Contacto
+                    </div>
+                    <div className=" py-2 text-gray-400 col-span-2">
+                      {myProfileData["userProps"]?.contact}
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
+                    <div className=" py-2 font-semibold text-[#28B0A2]">
+                      Email
+                    </div>
+                    <div className=" py-2 text-gray-400 col-span-2">
+                      {belloPerfil?.email}
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
+                    <div className=" py-2 font-semibold text-[#28B0A2]">
+                      Ciudad
+                    </div>
+                    <div className=" py-2 text-gray-400 col-span-2">
+                      {myProfileData["userProps"]?.city}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-						<div hidden={hidden} className="w-full">
-							{myPets?.length > 0 ? (
-								<BadgesPets
-									user={user}
-									hidden={hidden}
-									setHidden={setHidden}
-									handleActiveEditDog={handleActiveEditDog}
-									setOrder={setOrder}
-								/>
-							) : null}
-						</div>
-						<div>
-							<Transactions
-								transactions={transactions}
-								setOrder={setOrder}
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-			{activeModalEditDog && (
-				<ModalEditDog
-					dataEditDog={dataEditDog}
-					handleActiveEditDog={handleActiveEditDog}
-				/>
-			)}
-			<div>
-				<Footer />
-			</div>
-		</div>
-	);
+            <div hidden={hidden} className="w-full">
+              {myPets?.length > 0 ? (
+                <BadgesPets
+                  user={user}
+                  hidden={hidden}
+                  setHidden={setHidden}
+                  handleActiveEditDog={handleActiveEditDog}
+                  setOrder={setOrder}
+                  setActiveModalEditDog={setActiveModalEditDog}
+                />
+              ) : null}
+            </div>
+            <div>
+              <Transactions transactions={transactions} setOrder={setOrder} />
+            </div>
+          </div>
+        </div>
+      </div>
+      {activeModalEditDog && (
+        <ModalEditDog
+          dataEditDog={dataEditDog}
+          handleActiveEditDog={handleActiveEditDog}
+        />
+      )}
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default NuevoProfile;
