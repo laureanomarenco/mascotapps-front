ed09d6326d01033d8c1b89be6f952872430e185a
Gonzalo <fonsofara@gmail.com>
2022-09-26 00:36:12 -0300

    basta por hoy

--- src/components/UserProfile/Transactions/Reviews.jsx
+++ src/components/UserProfile/Transactions/Reviews.jsx
@@ -22,24 +22,13 @@ const Reviews = ({ userProf }) => {
                   key={r.id}
                 >
                   <div className="flex h-12 w-12 items-center align-center justify-center rounded-full border border-blue-100 bg-blue-50">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6 text-blue-400"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                      />
-                    </svg>
+                    <img src={r.reviewer_image} alt="img" />
                   </div>
                   <div className="ml-4">
-                    <Rating name="read-only" value={r.stars} readOnly />
-                    <h2 className="font-semibold">Nombre</h2>
+                    <Rating name="read-only" value={r.starts} readOnly />
+                    <h2 className="font-semibold capitalize">
+                      {r.reviewer_name.toLowerCase()}
+                    </h2>
                     <p className="mt-2 text-sm text-gray-500">{r.comments}</p>
                   </div>
                 </div>
