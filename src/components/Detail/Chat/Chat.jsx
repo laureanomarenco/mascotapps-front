b0cf401216cff7efff521450651d14b5c0b2dc1f
Gonzalo <fonsofara@gmail.com>
2022-09-27 19:38:23 -0300

    chatbot sin estilos

--- src/components/Detail/Chat/Chat.jsx
+++ src/components/Detail/Chat/Chat.jsx
@@ -8,37 +8,41 @@ class Review extends Component {
 
     this.state = {
       name: "",
-      gender: "",
-      age: "",
+      where: "",
+      contact: "",
     };
   }
 
   componentDidMount() {
     const { steps } = this.props;
-    const { name, gender, age } = steps;
+    const { name, where, contact } = steps;
 
-    this.setState({ name, gender, age });
+    this.setState({ name, where, contact });
   }
 
   render() {
-    const { name, gender, age } = this.state;
-    console.log(name.value, gender.value, age.value);
+    const { name, where, contact } = this.state;
+    console.log({
+      Nombre: name.value,
+      Donde: where.value,
+      Contacto: contact.value,
+    });
     return (
       <div style={{ width: "100%" }}>
-        <h3>Summary</h3>
+        <h3>Resumen</h3>
         <table>
           <tbody>
             <tr>
-              <td>Name</td>
+              <td>Nombre</td>
               <td>{name.value}</td>
             </tr>
             <tr>
-              <td>Gender</td>
-              <td>{gender.value}</td>
+              <td>Dónde</td>
+              <td>{where.value}</td>
             </tr>
             <tr>
-              <td>Age</td>
-              <td>{age.value}</td>
+              <td>Contacto</td>
+              <td>{contact.value}</td>
             </tr>
           </tbody>
         </table>
@@ -62,99 +66,104 @@ class SimpleForm extends Component {
         steps={[
           {
             id: "1",
-            message: "Cual es tu nombre?",
+            message: "¿Cuál es tu nombre?",
             trigger: "name",
           },
           {
             id: "name",
             user: true,
-            trigger: "2",
+            trigger: "3",
           },
           {
-            id: "2",
-            message: "Hola, {previousValue}! ¿Crees que viste a esta mascota?",
-            trigger: "vista",
+            id: "3",
+            message: "Hola {previousValue}! ¿Dónde viste esta mascota?",
+            trigger: "where",
           },
           {
-            id: "vista",
-            options: [
-              { value: "Si", label: "Si", trigger: "5" },
-              { value: "No", label: "No", trigger: "6" },
-            ],
+            id: "where",
+            user: true,
+            trigger: "5",
           },
           {
             id: "5",
-            message: "¿Dónde crees haberla visto?",
+            message:
+              "Deja un número para que el anunciante se ponga en contacto",
             trigger: "contact",
           },
           {
-            id: "6",
-            message: "Gracias por tu ayuda!",
-            end: true,
-          },
-          {
             id: "contact",
-            message: "Deja un número de contacto",
+            user: true,
             trigger: "7",
+            validator: (value) => {
+              if (isNaN(value)) {
+                return "debe ser un número";
+              } else if (value.length < 10) {
+                return "value must be positive";
+              }
+              return true;
+            },
           },
           {
             id: "7",
-            user: true,
+            message: "Genial! Veamos un resumen de los datos ingresados",
             trigger: "review",
           },
           {
             id: "review",
             component: <Review />,
             asMessage: true,
-            trigger: "end-message",
+            trigger: "update",
           },
-          // {
-          //   id: "update",
-          //   message: "Would you like to update some field?",
-          //   trigger: "update-question",
-          // },
-          // {
-          //   id: "update-question",
-          //   options: [
-          //     { value: "yes", label: "Yes", trigger: "update-yes" },
-          //     { value: "no", label: "No", trigger: "end-message" },
-          //   ],
-          // },
-          // {
-          //   id: "update-yes",
-          //   message: "What field would you like to update?",
-          //   trigger: "update-fields",
-          // },
-          // {
-          //   id: "update-fields",
-          //   options: [
-          //     { value: "name", label: "Name", trigger: "update-name" },
-          //     { value: "gender", label: "Gender", trigger: "update-gender" },
-          //     { value: "age", label: "Age", trigger: "update-age" },
-          //   ],
-          // },
-          // {
-          //   id: "update-name",
-          //   update: "name",
-          //   trigger: "7",
-          // },
-          // {
-          //   id: "update-gender",
-          //   update: "gender",
-          //   trigger: "7",
-          // },
-          // {
-          //   id: "update-age",
-          //   update: "age",
-          //   trigger: "7",
-          // },
+          {
+            id: "update",
+            message: "¿Los datos son correctos?",
+            trigger: "update-question",
+          },
+          {
+            id: "update-question",
+            options: [
+              { value: "no", label: "No", trigger: "update-yes" },
+              { value: "si", label: "Si", trigger: "end-message" },
+            ],
+          },
+          {
+            id: "update-yes",
+            message: "¿Qué datos deseas modificar?",
+            trigger: "update-fields",
+          },
+          {
+            id: "update-fields",
+            options: [
+              { value: "name", label: "name", trigger: "update-name" },
+              { value: "where", label: "where", trigger: "update-where" },
+              {
+                value: "contact",
+                label: "contact",
+                trigger: "update-contact",
+              },
+            ],
+          },
+          {
+            id: "update-name",
+            update: "name",
+            trigger: "7",
+          },
+          {
+            id: "update-where",
+            update: "where",
+            trigger: "7",
+          },
+          {
+            id: "update-contact",
+            update: "contact",
+            trigger: "7",
+          },
           {
             id: "end-message",
-            message: "Thanks! Your data was submitted successfully!",
+            message: "Gracias! Tus datos han sido enviados!",
             end: true,
           },
         ]}
-        hideInput={true}
       />
     );
   }
