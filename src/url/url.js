a6783a8a9be23cce4b02474f260af483811237b2
alejandro juka <alejandrojuka2@gmail.com>
2022-09-29 11:00:19 -0300

    cambiando url

--- src/url/url.js
+++ src/url/url.js
@@ -1,4 +1,4 @@
-export const URL = "https://worker-dev-push.up.railway.app/";
+export const URL = "https://juka-production.up.railway.app/";
 export const URL_CIUDAD_API =
   "https://apis.datos.gob.ar/georef/api/municipios?max=2500";
 
@@ -7,12 +7,12 @@ export const ALLPETS = URL + "pets";
 export const PET_DETAIL = URL + "pets/";
 export const SEARCH_BY = URL + "pets/search?input=";
 export const DONATION = URL + "checkout/balance";
-export const CREAT = "https://worker-dev-push.up.railway.app/users/newuser";
+export const CREAT = "https://juka-production.up.railway.app/users/newuser";
 export const GET_MY_PETS =
-  "https://worker-dev-push.up.railway.app/users/getallpetsofuser";
+  "https://juka-production.up.railway.app/users/getallpetsofuser";
 export const PET_SPECIES = URL + "pets/especies";
 // export const URL_POST_PET=URL_ROUTES + "users/postnewpet"
-export const POST = "https://worker-dev-push.up.railway.app/pets/postnewpet";
+export const POST = "https://juka-production.up.railway.app/pets/postnewpet";
 export const DELETE = URL + "users/deletepet";
 export const LOGIN = URL + "auth/google";
 export const LOGIN_LOGGED = URL + "auth/logged_in";
