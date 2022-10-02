c8d89749331a37f9fe392ab257606a541be8d119
Gonzalo <fonsofara@gmail.com>
2022-10-02 01:06:07 -0300

    flowchart imgs

--- src/components/FlowChart/FlowChart.jsx
+++ src/components/FlowChart/FlowChart.jsx
@@ -11,27 +11,27 @@ export default function FlowChart() {
         <Step
           title="Logueate"
           subtitle="Registrate o inicia sesión para acceder a todas las funcionalidades"
-          image="https://res.cloudinary.com/dfbxjt69z/image/upload/v1663276317/mascotapps/perrito_apwyz0.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664671059/pexels-cottonbro-5082561_mrkmah.png"
         />
         <Step
           title="Publicá"
           subtitle="Publicá un aviso, tenes opciones para postear una mascota."
-          image="https://res.cloudinary.com/dfbxjt69z/image/upload/v1663276317/mascotapps/perrito_apwyz0.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664676180/Screenshot_2022-10-01_at_22-20-37_Mascotapp_jbcebl.png"
         />
         <Step
           title="Ponete en contacto"
           subtitle="Desde el detalle de una mascota podés acceder a los datos del usuario que la publicó."
-          image="https://res.cloudinary.com/dfbxjt69z/image/upload/v1663276317/mascotapps/perrito_apwyz0.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664678958/contact_ikuulp.png"
         />
         <Step
           title="Lleva tu mascota a casa"
           subtitle="Lleva a tu nueva mascota a casa y calificá al usuario con el que interactuaste."
-          image="https://res.cloudinary.com/dfbxjt69z/image/upload/v1663276317/mascotapps/perrito_apwyz0.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664679821/pethome_xhqbu9.png"
         />
         <Step
           title="Sumá puntos"
           subtitle="Con cada interacción que tengas con otros usuarios, sumás puntos. Esos puntos podes canjearlos"
-          image="https://res.cloudinary.com/dfbxjt69z/image/upload/v1663276317/mascotapps/perrito_apwyz0.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664682700/mascopoints_c10w0o.png"
         />
       </div>
     </div>
