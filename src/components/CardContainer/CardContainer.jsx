e1d569a8974f02ea23411cca4330f956275e25ae
laureanomarenco <laureanomarenco@gmail.com>
2022-10-03 21:39:00 -0300

    images comprimidas y titulo cardcontainer

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -8,68 +8,70 @@ import Swal from "sweetalert2";
 import SortBy from "../SortBy/SortBy";
 
 export default function CardContainer() {
-	const dispatch = useDispatch();
-	const loading = useSelector(state => state.isLoading);
-	const pets = useSelector(state => state.allPets);
-	const searchedPets = useSelector(state => state.searchedPets);
-	const notFound = useSelector(state => state.notFound);
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.isLoading);
+  const pets = useSelector((state) => state.allPets);
+  const searchedPets = useSelector((state) => state.searchedPets);
+  const notFound = useSelector((state) => state.notFound);
 
-	const [page, setPage] = useState(1);
-	const showPerPage = 6;
-	const lastOnPage = page * showPerPage;
-	const firstOnPage = lastOnPage - showPerPage;
-	const showPets = pets.slice(firstOnPage, lastOnPage);
-	const showSearch = searchedPets.slice(firstOnPage, lastOnPage);
+  const [page, setPage] = useState(1);
+  const showPerPage = 6;
+  const lastOnPage = page * showPerPage;
+  const firstOnPage = lastOnPage - showPerPage;
+  const showPets = pets.slice(firstOnPage, lastOnPage);
+  const showSearch = searchedPets.slice(firstOnPage, lastOnPage);
 
-	function pagination(pageNumber) {
-		setPage(pageNumber);
-	}
+  function pagination(pageNumber) {
+    setPage(pageNumber);
+  }
 
-	useEffect(() => {
-		!pets.length && dispatch(fetchPets());
-	}, [dispatch]);
+  useEffect(() => {
+    !pets.length && dispatch(fetchPets());
+  }, [dispatch]);
 
-	const showAlert = () => {
-		Swal.fire({
-			title: "UPS!",
-			text:
-				"Hubo un error en el servidor. Reintente recargando la página",
-			icon: "error",
-			confirmButtonText: "Recargar",
-		}).then(() => window.location.reload());
-	};
-	return (
-		<>
-			<div className="flex md:flex-row-reverse flex-col ">
-				<Pagination
-					pets={pets.length}
-					searchedPets={
-						searchedPets.length !== pets.length
-							? searchedPets.length
-							: pets.length
-					}
-					showPerPage={showPerPage}
-					page={page}
-					pagination={pagination}
-				/>
-				<SortBy />
-			</div>
-			<div
-				className={`${
-					loading
-						? "grid gap-1 grid-rows-1 pt-40"
-						: "grid gap-1 grid-rows-1 md:grid-cols-2 xl:grid-cols-3"
-				} min-h-[70vh]`}
-			>
-				{notFound && showAlert()}
-				{loading && !notFound ? (
-					<Spinner />
-				) : searchedPets.length ? (
-					showSearch?.map((pet, i) => <Card key={i} data={pet} />)
-				) : (
-					showPets?.map(pet => <Card key={pet.id} data={pet} />)
-				)}
-			</div>
-		</>
-	);
+  const showAlert = () => {
+    Swal.fire({
+      title: "UPS!",
+      text: "Hubo un error en el servidor. Reintente recargando la página",
+      icon: "error",
+      confirmButtonText: "Recargar",
+    }).then(() => window.location.reload());
+  };
+  return (
+    <>
+      <div className="flex md:flex-row-reverse flex-col ">
+        <p className="text-center my-8 text-4xl font-bold">
+          Mascotas activas actualmente
+        </p>
+        <Pagination
+          pets={pets.length}
+          searchedPets={
+            searchedPets.length !== pets.length
+              ? searchedPets.length
+              : pets.length
+          }
+          showPerPage={showPerPage}
+          page={page}
+          pagination={pagination}
+        />
+        <SortBy />
+      </div>
+      <div
+        className={`${
+          loading
+            ? "grid gap-1 grid-rows-1 pt-40"
+            : "grid gap-1 grid-rows-1 md:grid-cols-2 xl:grid-cols-3"
+        } min-h-[70vh]`}
+      >
+        {notFound && showAlert()}
+        {loading && !notFound ? (
+          <Spinner />
+        ) : searchedPets.length ? (
+          showSearch?.map((pet, i) => <Card key={i} data={pet} />)
+        ) : (
+          showPets?.map((pet) => <Card key={pet.id} data={pet} />)
+        )}
+      </div>
+    </>
+  );
 }
--- src/components/FlowChart/FlowChart.jsx
+++ src/components/FlowChart/FlowChart.jsx
@@ -11,22 +11,22 @@ export default function FlowChart() {
         <Step
           title="Logueate"
           subtitle="Regístrate o inicia sesión para acceder a todas las funcionalidades"
-          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664801948/pexels-cottonbro-5082561_ni6bve.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664843405/pexels-cbve_vngzrm.jpg"
         />
         <Step
           title="Publica"
           subtitle="Publica un aviso, tenés distintas opciones para hacer más preciso el posteo de una mascota."
-          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664801967/postea_bxviwz.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664843405/postea_bx_sl2g9p.jpg"
         />
         <Step
           title="Ponete en contacto"
           subtitle="Desde el detalle de una mascota podrás acceder a los datos del usuario que realizó la publicación."
-          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664801935/contact_t19mfs.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664843405/contact_t1_np4le3.jpg"
         />
         <Step
           title="Lleva tu mascota a casa"
           subtitle="Reúnete y lleva tu mascota a casa. Califica al usuario con el que interactuaste."
-          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664801941/pethome_g2nx7z.png"
+          image="https://res.cloudinary.com/dpxrr2uyq/image/upload/v1664843405/pethome_g_bw9nci.jpg"
         />
         <Step
           title="Suma puntos"
