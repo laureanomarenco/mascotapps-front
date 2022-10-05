d024bbd1b24e3a21830ab8fb3f8c92a516a3e10c
lucascolm <lucaspaganelli20@gmail.com>
2022-10-05 16:41:03 -0300

    + cambiosss

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams,useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import {
   getDetail,
@@ -18,19 +18,20 @@ import { TbVaccine } from "react-icons/tb";
 import { BiMap } from "react-icons/bi";
 import { BsExclamationSquare } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
-import EndPost from "./EndPost";
+
 import Chat from "./Chat/Chat";
 
 export default function Detail() {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const pet = useSelector((state) => state.pet);
   const loading = useSelector((state) => state.isLoading);
   const userContact = useSelector((state) => state.publicUserDetail);
 
   const { user, isAuthenticated, isLoading } = useAuth0();
   const [hidden, setHidden] = useState(true);
-  const [hiddenEnd, setHiddenEnd] = useState(true);
+  // const [hiddenEnd, setHiddenEnd] = useState(true);
 
   useEffect(() => {
     dispatch(getDetail(id));
@@ -232,19 +233,12 @@ export default function Detail() {
                   (
                   <div>
                     <button
-                      onClick={()=>setHidden(false)}
+                      onClick={ ()=>navigate('/account')}
                       className="text-black bg-[#ffd803] hover:bg-[#ffd803]/80 focus:ring-2 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2 gap-6 "
                     >
                       Dar por concretada la publicación
                     </button>
-                    <div hidden={false} className="w-full">
-                      <EndPost
-                        user={userContact}
-                        hiddenEnd={hiddenEnd}
-                        setHiddenEnd={setHiddenEnd}
-                        idPet={id}
-                      />
-                    </div>
+                    
                   </div>
                 ) : pet.postStatus === 'cancelado' ? <p className="text-xl font-bold text-red-700">Has cancelado esta publicación</p>
                   : <p className="text-xl font-bold text-teal-700">Has concretado esta publicación</p> 
